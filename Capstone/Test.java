6805a4fce17e39fa02f69cdcdcd6cfe3329f9d51
shopgirl052202 <159318249+shopgirl052202@users.noreply.github.com>
2024-02-07 12:01:03 -0700

    added files
    added files

---
+++ Capstone/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world");
+
+	}
+
+}
